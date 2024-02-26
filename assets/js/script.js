1ec1c2856f5e030ec17743f678039eb651399d80
Satyaa-Mandalapu <satyamandalapu312@gmail.com>
2024-02-26 13:58:27 -0600

    html js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["SAP SD Anaylst"],
+    strings: ["SAP SD Analyst"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
